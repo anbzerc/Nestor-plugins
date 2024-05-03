b271ebeb9de81002c06f204756f66edb7d53c6d9
Caelzero <66829188+anbzerc@users.noreply.github.com>
2024-05-03 12:29:31 +0200

    Add required queue to OpenDeezer

--- Plugins/OpenDeezer/__init__.py
+++ Plugins/OpenDeezer/__init__.py
@@ -2,9 +2,9 @@ from PluginCore import PluginModel
 from subprocess import run
 
 class Plugin(PluginModel):
-    def run(self, plateform="flatpack"):
+    def run(self, must_continue_queue, text_datas_queue, plateform="flatpak"):
         if plateform == "flatpak":
             result = run("flatpak run dev.aunetx.deezer & disown %%", shell=True, timeout=10)
-
+        must_continue_queue.put(False)
     def test(self):
         self.run()
